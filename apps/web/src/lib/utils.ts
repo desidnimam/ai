be70f05b3456cd340a9da1929b07ff1cf6f2b3f7
Ali Imam <visual.imam@gmail.com>
2024-07-16 12:34:49 +0530

    turant

--- apps/web/src/lib/utils.ts
+++ apps/web/src/lib/utils.ts
@@ -3,6 +3,10 @@ import type { ClassValue } from "clsx";
 import { clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
 
+export function truncate(str: string, length: number) {
+  return str.length > length ? `${str.substring(0, length)}...` : str;
+}
+
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
